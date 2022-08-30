64466bb6625f82da77e880420586fbeaa1bec506
Madhav Gupta <103241086+guptamadhav07@users.noreply.github.com>
2022-08-30 19:56:02 +0530

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Contact.css'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Faq from './Faq';
+const Contact = () => {
+  return (
+    <>
+        <section id="contact">
+  <div className="contact-box">
+    <div className="contact-links">
+      <h2>CONTACT</h2>
+      
+    </div>
+    <div className="contact-form-wrapper">
+      <form>
+      {/* <h2>Contact</h2> */}
+        <div className="form-item">
+          <input type="text" name="sender" required />
+          <label>Name:</label>
+        </div>
+        <div className="form-item">
+          <input type="text" name="email" required />
+          <label>Email:</label>
+        </div>
+        <div className="form-item">
+          <textarea className="" name="message" required />
+          <label>Message:</label>
+        </div>
+        <button className="submit-btn">Send</button>
+        <div className="links">
+        <div className="link">
+          <a><LinkedInIcon/> </a>
+        </div>
+        <div className="link">
+          <a><FacebookIcon/> </a>
+        </div>
+        <div className="link">
+          <a><InstagramIcon/> </a>
+        </div>
+        <div className="link">
+          <a><TwitterIcon/> </a>
+        </div>
+      </div>  
+      </form>
+    </div>
+  </div>
+</section>
+<Faq/>
+
+    </>
+  )
+}
+
+export default Contact
